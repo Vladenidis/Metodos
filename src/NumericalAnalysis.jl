36eca8daebf2e4949ad9df17a04573239cc62ada
Vladenidis <163321151+Vladenidis@users.noreply.github.com>
2024-03-20 11:59:24 -0300

    Add files via upload

---
+++ src/NumericalAnalysis.jl
@@ -0,0 +1,9 @@
+module NumericalAnalysis
+
+include("Includes.jl")
+
+export round_sum, round_mul, trunc_sum, trunc_mul, absolute_error, relative_error, approximated_value_interval_given_relative_error
+
+greet() = print("Hello World!")
+
+end # module NumericalAnalysis
